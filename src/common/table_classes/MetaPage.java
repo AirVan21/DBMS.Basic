75b53fd8e5fd20c026484ce5bd1745bcc59e13a8
AirVan21 <airvan21@gmail.com>
2015-10-17 22:35:08 +0300

    Added MetaPage class

---
+++ src/common/table_classes/MetaPage.java
@@ -0,0 +1,10 @@
+package common.table_classes;
+
+/**
+ * Created by airvan21 on 17.10.15.
+ */
+public class MetaPage {
+    Integer recordLength;
+    Integer firstPageWithSpace;
+    Integer firstPageWithNoSpace;
+}
